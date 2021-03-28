e32b2d48d6aaf5145ec8f82b14af94ae6fe59c2d
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-03-29 07:45:04 +0900

    [mod] 引数の変数化

--- scripts/cipher_plaintext.sh
+++ scripts/cipher_plaintext.sh
@@ -5,4 +5,4 @@ KEYRING=$2
 KEY=$3
 LOCATION=$4
 
-echo -n $1 | gcloud kms encrypt --project $(gcloud config get-value core/project) --keyring $2 --key $3 --location $4  --plaintext-file - --ciphertext-file - | base64
+echo -n $PLAINTEXT | gcloud kms encrypt --project $(gcloud config get-value core/project) --keyring $KEYRING --key $KEY --location $LOCATION  --plaintext-file - --ciphertext-file - | base64
